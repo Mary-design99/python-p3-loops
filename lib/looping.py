aa1fed233802e60086b87ddca13d95e382ca1cec
Mary-design99 <mary.nyarangi@student.moringaschool.com>
2025-08-05 23:08:04 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,33 @@
 
 def happy_new_year():
     # code goes here!
+    counter=10
+    while counter>0:
+        print(counter) 
+        counter=counter - 1 # or counter -= 1
+    print("Happy New Year!")
     pass
-
+happy_new_year()
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list=[]
+    for integer in int_list:
+        squared_list.append(integer * integer)
+    return squared_list
+print(square_integers([1,2,3,4,5]))   
+   # pass
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 ==0 and num % 5 ==0:
+            print("FizzBuzz")
+        elif num % 3 ==0:
+            print("Fizz") 
+        elif num % 5 ==0:
+            print("Buzz")
+        else:
+            print(num)
+
+fizzbuzz()
+    #pass
